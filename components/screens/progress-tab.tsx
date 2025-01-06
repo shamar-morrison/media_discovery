8e642fedb7a716e5c152d7e338f7a592af829242
Shamar Morrison <shamar.webdev@gmail.com>
2025-01-06 12:52:03 -0500

    fix marking episode as unwatched

--- components/screens/progress-tab.tsx
+++ components/screens/progress-tab.tsx
@@ -1,8 +1,12 @@
 import { View } from "react-native";
 import { useCallback, useState } from "react";
-import { useWatchedEpisodesStore } from "@/store/watched-episodes-store";
+import {
+  StartedShows,
+  useWatchedEpisodesStore,
+} from "@/store/watched-episodes-store";
 import { ThemedText } from "@/components/themed-text";
 import { useFocusEffect } from "@react-navigation/native";
+import { FlashList } from "@shopify/flash-list";
 
 export function ProgressTab() {
   const [_, setForceUpdate] = useState(0);
@@ -19,14 +23,45 @@ export function ProgressTab() {
   );
 
   return (
-    <View>
-      {startedShows.map((show) => {
-        return (
-          <View key={show.showId}>
-            <ThemedText>{show.showName}</ThemedText>
-          </View>
-        );
-      })}
+    <View className={"h-full"}>
+      <FlashList
+        estimatedItemSize={startedShows.length}
+        data={startedShows}
+        className={"mt-4"}
+        canCancelContentTouches={false}
+        renderItem={({ item }) => <TvShowProgressCard {...item} />}
+      />
+    </View>
+  );
+}
+
+function TvShowProgressCard({
+  showId,
+  showName,
+  totalWatchedEpisodes,
+  lastWatchedEpisode,
+  lastWatchedAt,
+}: StartedShows) {
+  const getNextEpisodeToWatch = useWatchedEpisodesStore(
+    (state) => state.getNextEpisodeToWatch,
+  );
+
+  const nextEpisodeToWatch = getNextEpisodeToWatch(showId);
+  return (
+    <View
+      className={"flex gap-3 rounded-xl bg-black-100 p-7 w-[90%] mx-auto h-60"}
+    >
+      <ThemedText className={"font-inter-semibold opacity-30 text-lg"}>
+        {showName}
+      </ThemedText>
+      <View className="flex flex-row">
+        <ThemedText>{nextEpisodeToWatch?.episodeNumber}</ThemedText>
+        <ThemedText>{nextEpisodeToWatch?.episodeName}</ThemedText>
+        {/*<ThemedText className={"text-2xl font-inter-semibold"}>*/}
+        {/*  {lastWatchedEpisode?.seasonNumber}x*/}
+        {/*  {lastWatchedEpisode?.episodeNumber || "Not found"}*/}
+        {/*</ThemedText>*/}
+      </View>
     </View>
   );
 }
