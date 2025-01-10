b06b9c1b0a84315272b0395b22f5c6805c427428
Shamar Morrison <shamar.webdev@gmail.com>
2025-01-10 03:20:23 -0500

    add Discover title

--- app/(root)/(tabs)/(home)/index.tsx
+++ app/(root)/(tabs)/(home)/index.tsx
@@ -3,31 +3,39 @@ import { LatestMoviesTab } from "@/components/latest-movies-tab";
 import { TabBarLabel } from "@/components/tab-bar-label";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { LatestTvShowsTab } from "@/components/latest-tv-shows-tab";
+import React from "react";
+import { ScreenTitle } from "@/components/screen-title";
+import { View } from "react-native";
 
 const Tab = createMaterialTopTabNavigator();
 
 // home screen
 export default function Index() {
   return (
-    <Tab.Navigator screenOptions={tabStyles} backBehavior={"none"}>
-      <Tab.Screen
-        name="movies"
-        component={LatestMoviesTab}
-        options={{
-          tabBarLabel: ({ focused }) => (
-            <TabBarLabel focused={focused}>Latest Movies</TabBarLabel>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="tv"
-        component={LatestTvShowsTab}
-        options={{
-          tabBarLabel: ({ focused }) => (
-            <TabBarLabel focused={focused}>Latest TV Shows</TabBarLabel>
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <>
+      <View className="px-4 pt-4">
+        <ScreenTitle>Discover</ScreenTitle>
+      </View>
+      <Tab.Navigator screenOptions={tabStyles} backBehavior={"none"}>
+        <Tab.Screen
+          name="movies"
+          component={LatestMoviesTab}
+          options={{
+            tabBarLabel: ({ focused }) => (
+              <TabBarLabel focused={focused}>Latest Movies</TabBarLabel>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="tv"
+          component={LatestTvShowsTab}
+          options={{
+            tabBarLabel: ({ focused }) => (
+              <TabBarLabel focused={focused}>Latest TV Shows</TabBarLabel>
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </>
   );
 }
