a765cf1f9f35d0aa2af025988e63ea7d41e0daa7
Shamar Morrison <shamar.webdev@gmail.com>
2025-01-17 17:05:20 -0500

    remove drawer nav

--- app/(root)/(tabs)/(home)/_layout.tsx
+++ app/(root)/(tabs)/(home)/_layout.tsx
@@ -1,30 +1,3 @@
-import React from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { Drawer } from "expo-router/drawer";
+import { SharedLayout } from "@/components/shared-layout/shared-layout";
 
-export default function HomeLayout() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer
-        screenOptions={{
-          drawerItemStyle: { display: "none" },
-          headerShown: false,
-          drawerStyle: {
-            backgroundColor: "#1d1d1d",
-          },
-          drawerLabelStyle: {
-            color: "#fff",
-          },
-        }}
-      >
-        <Drawer.Screen
-          name="index"
-          options={{
-            drawerLabel: "Discover",
-            drawerItemStyle: { display: "flex" },
-          }}
-        />
-      </Drawer>
-    </GestureHandlerRootView>
-  );
-}
+export default SharedLayout;
--- components/drawer-menu-button.tsx
+++ components/drawer-menu-button.tsx
@@ -1,18 +1,11 @@
 import { TouchableOpacity } from "react-native";
 import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { useNavigation } from "expo-router";
-import { DrawerActions } from "@react-navigation/native";
 import { hitSlop } from "@/utils/hit-slop";
 
 export function DrawerMenuButton() {
-  const navigation = useNavigation();
   return (
-    <TouchableOpacity
-      className={"mt-[3px]"}
-      hitSlop={hitSlop}
-      onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-    >
+    <TouchableOpacity className={"mt-[3px]"} hitSlop={hitSlop}>
       <Ionicons name="menu" size={27} color="#fff" className={"mr-2"} />
     </TouchableOpacity>
   );
