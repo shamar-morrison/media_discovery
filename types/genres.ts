ef136cf54826be3dddfb5c0d1cb07adcd6b23ba1
Shamar Morrison <shamar.webdev@gmail.com>
2025-01-17 00:06:01 -0500

    added genres

---
+++ types/genres.ts
@@ -0,0 +1,45 @@
+interface Genre {
+  id: number;
+  name: string;
+}
+
+export const MOVIE_GENRES: { [key: string]: Genre } = {
+  ACTION: { id: 28, name: "Action" },
+  ADVENTURE: { id: 12, name: "Adventure" },
+  ANIMATION: { id: 16, name: "Animation" },
+  COMEDY: { id: 35, name: "Comedy" },
+  CRIME: { id: 80, name: "Crime" },
+  DOCUMENTARY: { id: 99, name: "Documentary" },
+  DRAMA: { id: 18, name: "Drama" },
+  FAMILY: { id: 10751, name: "Family" },
+  FANTASY: { id: 14, name: "Fantasy" },
+  HISTORY: { id: 36, name: "History" },
+  HORROR: { id: 27, name: "Horror" },
+  MUSIC: { id: 10402, name: "Music" },
+  MYSTERY: { id: 9648, name: "Mystery" },
+  ROMANCE: { id: 10749, name: "Romance" },
+  SCIENCE_FICTION: { id: 878, name: "Science Fiction" },
+  TV_MOVIE: { id: 10770, name: "TV Movie" },
+  THRILLER: { id: 53, name: "Thriller" },
+  WAR: { id: 10752, name: "War" },
+  WESTERN: { id: 37, name: "Western" },
+} as const;
+
+export const TV_GENRES: { [key: string]: Genre } = {
+  ACTION_ADVENTURE: { id: 10759, name: "Action & Adventure" },
+  ANIMATION: { id: 16, name: "Animation" },
+  COMEDY: { id: 35, name: "Comedy" },
+  CRIME: { id: 80, name: "Crime" },
+  DOCUMENTARY: { id: 99, name: "Documentary" },
+  DRAMA: { id: 18, name: "Drama" },
+  FAMILY: { id: 10751, name: "Family" },
+  KIDS: { id: 10762, name: "Kids" },
+  MYSTERY: { id: 9648, name: "Mystery" },
+  NEWS: { id: 10763, name: "News" },
+  REALITY: { id: 10764, name: "Reality" },
+  SCIFI_FANTASY: { id: 10765, name: "Sci-Fi & Fantasy" },
+  SOAP: { id: 10766, name: "Soap" },
+  TALK: { id: 10767, name: "Talk" },
+  WAR_POLITICS: { id: 10768, name: "War & Politics" },
+  WESTERN: { id: 37, name: "Western" },
+} as const;
