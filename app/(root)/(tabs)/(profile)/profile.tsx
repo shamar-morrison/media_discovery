f9ce7b97de10292ef4233b8fe5cd2823a8aad59d
Shamar Morrison <shamar.webdev@gmail.com>
2025-01-25 01:25:24 -0500

    improve trending profile tabs perf

--- app/(root)/(tabs)/(profile)/profile.tsx
+++ app/(root)/(tabs)/(profile)/profile.tsx
@@ -1,14 +1,14 @@
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { ScreenTitle } from "@/components/screen-title";
+import { ProgressTab } from "@/components/screens/progress-tab";
 import { WatchlistsTab } from "@/components/screens/watchlists-tab";
 import { TabBarLabel } from "@/components/tab-bar-label";
-import { tabStyles } from "@/styles/tab-styles";
-import { ProgressTab } from "@/components/screens/progress-tab";
-import React from "react";
-import { ScreenTitle } from "@/components/screen-title";
-import { Pressable, View } from "react-native";
 import { WatchlistExportImportSheet } from "@/components/watchlist-export-import-sheet";
+import { tabStyles } from "@/styles/tab-styles";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { router } from "expo-router";
+import React from "react";
+import { Pressable, View } from "react-native";
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -24,7 +24,16 @@ export default function Profile() {
           </Pressable>
         </View>
       </View>
-      <Tab.Navigator screenOptions={tabStyles} backBehavior={"none"}>
+      <Tab.Navigator
+        screenOptions={{
+          ...tabStyles,
+          lazy: true,
+          lazyPreloadDistance: 1,
+          tabBarPressColor: "transparent",
+          animationEnabled: true,
+        }}
+        backBehavior={"none"}
+      >
         <Tab.Screen
           name="lists"
           component={WatchlistsTab}
