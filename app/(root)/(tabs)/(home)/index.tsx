ea7cb1b9bba6df794d2703f53e1a68738e5a096b
Shamar Morrison <shamar.webdev@gmail.com>
2025-01-24 20:30:58 -0500

    improve home screen tabs perf

--- app/(root)/(tabs)/(home)/index.tsx
+++ app/(root)/(tabs)/(home)/index.tsx
@@ -1,12 +1,12 @@
-import { tabStyles } from "@/styles/tab-styles";
-import { MoviesTab } from "@/components/movies-tab";
-import { TabBarLabel } from "@/components/tab-bar-label";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { LatestTvShowsTab } from "@/components/latest-tv-shows-tab";
-import React from "react";
-import { ScreenTitle } from "@/components/screen-title";
-import { View } from "react-native";
 import { DrawerMenuButton } from "@/components/drawer-menu-button";
+import { LatestTvShowsTab } from "@/components/latest-tv-shows-tab";
+import { MoviesTab } from "@/components/movies-tab";
+import { ScreenTitle } from "@/components/screen-title";
+import { TabBarLabel } from "@/components/tab-bar-label";
+import { tabStyles } from "@/styles/tab-styles";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import React from "react";
+import { View } from "react-native";
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -18,7 +18,24 @@ export default function Index() {
         <ScreenTitle>Discover</ScreenTitle>
         <DrawerMenuButton />
       </View>
-      <Tab.Navigator screenOptions={tabStyles} backBehavior={"none"}>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarScrollEnabled: tabStyles.tabBarScrollEnabled,
+          tabBarIndicatorStyle: {
+            backgroundColor: "#fff",
+            height: 2,
+          },
+          tabBarActiveTintColor: tabStyles.tabBarActiveTintColor,
+          tabBarInactiveTintColor: tabStyles.tabBarInactiveTintColor,
+          tabBarStyle: tabStyles.tabBarStyle,
+          sceneStyle: tabStyles.sceneStyle,
+          animationEnabled: true,
+          swipeEnabled: true,
+          tabBarPressOpacity: 1,
+          tabBarPressColor: "transparent",
+        }}
+        backBehavior={"none"}
+      >
         <Tab.Screen
           name="movies"
           children={() => <MoviesTab />}
