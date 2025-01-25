f507c68b631f2e0ec3fe9273d187b9496f7e95c9
Shamar Morrison <shamar.webdev@gmail.com>
2025-01-24 20:37:21 -0500

    improve trending screen tabs perf

--- app/(root)/(tabs)/(trending)/trending.tsx
+++ app/(root)/(tabs)/(trending)/trending.tsx
@@ -1,11 +1,11 @@
-import { tabStyles } from "@/styles/tab-styles";
-import { TabBarLabel } from "@/components/tab-bar-label";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import React from "react";
-import { ScreenTitle } from "@/components/screen-title";
-import { View } from "react-native";
 import TrendingMovies from "@/app/(root)/(tabs)/(home)/trending-movies";
 import TrendingTV from "@/app/(root)/(tabs)/(home)/trending-tv";
+import { ScreenTitle } from "@/components/screen-title";
+import { TabBarLabel } from "@/components/tab-bar-label";
+import { tabStyles } from "@/styles/tab-styles";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import React from "react";
+import { View } from "react-native";
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -15,7 +15,24 @@ export default function Trending() {
       <View className="flex flex-row px-4 pt-4 bg-black-200 justify-between">
         <ScreenTitle>Trending</ScreenTitle>
       </View>
-      <Tab.Navigator screenOptions={tabStyles} backBehavior={"none"}>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarScrollEnabled: tabStyles.tabBarScrollEnabled,
+          tabBarIndicatorStyle: {
+            backgroundColor: "#fff",
+            height: 2,
+          },
+          tabBarActiveTintColor: tabStyles.tabBarActiveTintColor,
+          tabBarInactiveTintColor: tabStyles.tabBarInactiveTintColor,
+          tabBarStyle: tabStyles.tabBarStyle,
+          sceneStyle: tabStyles.sceneStyle,
+          animationEnabled: true,
+          swipeEnabled: true,
+          tabBarPressOpacity: 1,
+          tabBarPressColor: "transparent",
+        }}
+        backBehavior={"none"}
+      >
         <Tab.Screen
           name="trending-movies"
           children={() => <TrendingMovies />}
