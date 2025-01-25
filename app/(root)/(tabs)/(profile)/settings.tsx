858a1e4f4a7bf1447ea0b07a6217cd8d491a2dd9
Shamar Morrison <shamar.webdev@gmail.com>
2025-01-25 01:57:54 -0500

    edit button style

--- app/(root)/(tabs)/(profile)/settings.tsx
+++ app/(root)/(tabs)/(profile)/settings.tsx
@@ -2,7 +2,7 @@ import { View } from "react-native";
 import { ThemedText } from "@/components/themed-text";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useSettingsStore } from "@/store/settings-store";
-import { Stack } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function SettingsScreen() {
   const resetTvShowRemovalPreference = useSettingsStore(
@@ -11,21 +11,17 @@ export default function SettingsScreen() {
 
   return (
     <View className="flex-1 bg-black-200">
-      <Stack.Screen options={{ title: "Settings" }} />
-
       <View className="p-4">
         <View className="bg-black-100 rounded-lg p-4">
           <ThemedText className="text-lg font-inter-semibold mb-4">
             TV Show Preferences
           </ThemedText>
 
-          <TouchableOpacity
-            onPress={resetTvShowRemovalPreference}
-            className="bg-gray-700 p-4 rounded-lg"
-          >
-            <ThemedText className="text-center">
-              Reset TV Show Removal Preference
-            </ThemedText>
+          <TouchableOpacity onPress={resetTvShowRemovalPreference}>
+            <View className="border border-gray-300/10 p-4 rounded-lg flex-row items-center gap-3">
+              <Ionicons name={"refresh"} size={20} color={"#fff"} />
+              <ThemedText>Reset TV Show Removal Preference</ThemedText>
+            </View>
           </TouchableOpacity>
         </View>
       </View>
