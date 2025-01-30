b288fa84f4993a36f6f8f3a0669208af40401cbf
Shamar Morrison <shamar.webdev@gmail.com>
2025-01-30 03:42:55 -0500

    increase hitslop

--- utils/hit-slop.ts
+++ utils/hit-slop.ts
@@ -1,8 +1,8 @@
 import { Insets } from "react-native";
 
 export const hitSlop: number | Insets | null | undefined = {
-  top: 30,
-  bottom: 30,
-  left: 30,
-  right: 30,
+  top: 40,
+  bottom: 40,
+  left: 40,
+  right: 40,
 };
